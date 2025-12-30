11ae80e051765219373c53009a2a09a99c799f38
KAIBO ZHANG <kaibozhang@KBMacBook-Pro.local>
2025-12-30 23:44:32 +1100

    add husky

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+const config = {
+    plugins: ["prettier-plugin-tailwindcss"],
+    tailwindFunctions: ["mergeTwClasses"],
+    tabWidth: 2,
+    printWidth: 80,
+    trailingComma: "all",
+
+}
+
+export default config;
